7e4652838f4782f037a74cf133e5a5a25d1d8bb3
helium01 <fajarw935@gmail.com>
2024-07-09 18:27:26 +0700

    perbaikan db

---
+++ app/Http/Controllers/LayananController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\layanan;
+use Illuminate\Http\Request;
+
+class LayananController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\layanan  $layanan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(layanan $layanan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\layanan  $layanan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(layanan $layanan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\layanan  $layanan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, layanan $layanan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\layanan  $layanan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(layanan $layanan)
+    {
+        //
+    }
+}
--- database/migrations/2024_07_08_143922_create_pengunjungs_table.php
+++ database/migrations/2024_07_08_143922_create_pengunjungs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateJenisMukenasTable extends Migration
+class CreatePengunjungsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateJenisMukenasTable extends Migration
      */
     public function up()
     {
-        Schema::create('jenis__mukenas', function (Blueprint $table) {
+        Schema::create('pengunjungs', function (Blueprint $table) {
             $table->id();
-            $table->string('id_Jenis');
-            // $table->id('Nama');
-            $table->integer('Harga');
+            $table->string('nama_pengunjung');
+            $table->string('alamat');
+            $table->string('no_hp');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateJenisMukenasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('jenis__mukenas');
+        Schema::dropIfExists('pengunjungs');
     }
 }
